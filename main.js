7861757fa07e1a9f80c89fef6bfc1391f364c341
potato3248 <128443827+potato3248@users.noreply.github.com>
2024-01-09 20:19:08 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+
+}
+function setup() {
+    canvas = creatCanvas(300,300);
+    canvas.center();
+}
+
+function draw() {
+
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+}
